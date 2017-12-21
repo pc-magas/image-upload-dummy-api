744c764ea83738e72ec72a4192b0561ebf8396e3
pc-magas <pc-magas@users.noreply.github.com>
2017-12-21 12:07:48 +0200

    Basic scaffolding

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express()
 const conf = require('./config/web');
 
-app.post()
+app.get('/',(req,res)=>{
+    res.send('Hello world!!!');
+});
 
 app.listen(conf.listen_port,()=>{
     console.log('Listening into http://localhost:'+conf.listen_port);
